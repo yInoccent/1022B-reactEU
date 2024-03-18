77406c3848e5a207f7592e416672d934336d8424
guilhermeterenciani <guilhermeterenciani@gmail.com>
2024-03-18 11:09:37 -0400

    Criação de um global.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Header from './componentes/Header'
 import Main from './componentes/Main'
 import Footer from './componentes/Footer'
-
+import './global.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header/>
