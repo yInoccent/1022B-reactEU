7dd2052ad07711407d5cd75eecfc39cc85dec5ec
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-03-20 09:44:27 -0400

    Atualização do Main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,6 +1,21 @@
+import './Main.css'
 function Main(){
     return(
-        <main>Main</main>
+        <main>
+            <div className="box-estudante">
+                <img src="https://github.com/guilhermeterenciani.png" alt="Foto Estudante"/>
+                <h3 className="nome-estudante">
+                    Guilherme Figueiredo Terenciani
+                </h3>
+                <p className="descricao-estudante">
+                    Professor do IFMS nas matérias de 
+                    Desenvolvimento / Desenvolvimento Web
+                </p>
+                <a href="https://github.com/guilhermeterenciani">
+                    GitHub
+                </a>
+            </div>
+        </main>
     )
 }
 export default Main
